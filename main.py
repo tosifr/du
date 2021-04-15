bb17191103b8826420278b1ce53ee9e492ff8390
tosifr <82577864+tosifr@users.noreply.github.com>
2021-04-15 11:26:38 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,15 @@
+a=("lol")
+b=("kys")
+e = 13
+
+if e < 13: basic.show_string(a)
+if e == 13: basic.show_string(b)
+if e > 13: basic.show_string("fu")
+
+
+
+
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,18 @@
+let a = "lol"
+let b = "kys"
+let e = 13
+if (e < 13) {
+    basic.showString(a)
+}
+
+if (e == 13) {
+    basic.showString(b)
+}
+
+if (e > 13) {
+    basic.showString("fu")
+}
+
+basic.forever(function on_forever() {
+    
+})
